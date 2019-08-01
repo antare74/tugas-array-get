009fda7c0ce914e2859aff210ad0473227be219f
antare74 <52792087+antare74@users.noreply.github.com>
2019-08-01 14:50:38 +0700

    menambah file latihan.php & pej.php
    latihan = data karyawan
    pej = list

---
+++ latihan.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PT. SunTech</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="header text-center">
+      <h1>Data Karyawan PT. SunTech</h1>
+    </div><br>
+
+<div class="container">
+  <div class="data">
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">NO</th>
+      <th scope="col">NAMA</th>
+      <th scope="col">NRP</th>
+      <th scope="col">BIO</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>090</td>
+      <td><a class="btn btn-primary btn-sm" href="#" role="button">Detail</a></td>
+    </tr>
+  </tbody>
+  </table>
+  </div>
+</div>
+
+  </body>
+</html>
---
+++ pej.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PT. SunTech</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style media="screen">
+      .row li{
+        font-size: 30px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="header text-center">
+      <h1>Detail Mark Profile</h1>
+    </div><br>
+
+<div class="container">
+<div class="row px-auto">
+  <div class="col-md-5 border"><img src="https://nowtoronto.com/downloads/34411/download/sachabaroncohen_468.jpg?cb=051cbcb557c3199b4e61d0989fccef2c" alt="No Image"> </div>
+  <div class="col-md-7 border p-5">
+    <ul>
+      <li>Nama Lengkap: Mark Abdullah</li>
+      <li>Nomor Telp. : 0829123124xx</li>
+      <li>Alamat: Cipondoh, crown c32</li>
+      <li>Pekerjaan: Enginering</li>
+    </ul>
+  </div>
+</div>
+<br><br>
+<a href="http://localhost/myweb/d4/latihan.php"><button type="button" class="btn btn-primary btn-sm" >Back To Home</button></a>
+</div>
+
+
+  </body>
+</html>
